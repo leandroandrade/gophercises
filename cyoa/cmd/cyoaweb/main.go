28d85f895263ba17751f47515cf9da5fa1b0e286
leandroandrade <leandro.andrade2588@gmail.com>
2018-08-07 21:52:36 -0300

    pattern

--- cyoa/cmd/cyoaweb/main.go
+++ cyoa/cmd/cyoaweb/main.go
@@ -7,6 +7,8 @@ import (
 	"github.com/leandroandrade/gophercises/cyoa"
 	"log"
 	"net/http"
+	"strings"
+	"html/template"
 )
 
 func main() {
@@ -26,8 +28,91 @@ func main() {
 		panic(err)
 	}
 
-	handler := cyoa.NewHandler(story)
+	//tpl := template.Must(template.New("").Parse("Hello!"))
+	//handler := cyoa.NewHandler(story, cyoa.WithTemplate(tpl))
+
+	//handler := cyoa.NewHandler(story)
+	//fmt.Printf("Starting the server on port: %d\n", *port)
+	//log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), handler))
+
+	customTemplate := template.Must(template.New("").Parse(customTemplateHTML))
+	h := cyoa.NewHandler(story,
+		cyoa.WithTemplate(customTemplate),
+		cyoa.WithPathFunc(customPrefixPath),
+	)
+
+	mux := http.NewServeMux()
+	mux.Handle("/story/", h)
 	fmt.Printf("Starting the server on port: %d\n", *port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), handler))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), mux))
 
 }
+
+func customPrefixPath(r *http.Request) string {
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "/story" || path == "/story/" {
+		path = "/story/intro"
+	}
+	return path[len("/story/"):]
+}
+
+var customTemplateHTML = `
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Choose Your Own Adventure **CUSTOM**</title>
+</head>
+<body>
+    <section class="page">
+        <h1>{{.Title}}</h1>
+        {{range .Paragraphs}}
+            <p>{{.}}</p>
+        {{end}}
+
+        <ul>
+        {{range .Options}}
+            <li><a href="/story/{{.Chapter}}">{{.Text}}</a></li>
+        {{end}}
+        </ul>
+    </section>
+    <style>
+        body {
+            font-family: helvetica, arial;
+        }
+        h1 {
+            text-align:center;
+            position:relative;
+        }
+        .page {
+            width: 80%;
+            max-width: 500px;
+            margin: 40px auto;
+            padding: 80px;
+            background: #FCF6FC;
+            border: 1px solid #eee;
+            box-shadow: 0 10px 6px -6px #797;
+        }
+        ul {
+            border-top: 1px dotted #ccc;
+            padding: 10px 0 0 0;
+            -webkit-padding-start: 0;
+        }
+        li {
+            padding-top: 10px;
+        }
+        a,
+        a:visited {
+            text-decoration: underline;
+            color: #555;
+        }
+        a:active,
+        a:hover {
+            color: #222;
+        }
+        p {
+            text-indent: 1em;
+        }
+    </style>
+</body>
+</html>`
--- cyoa/story.go
+++ cyoa/story.go
@@ -9,26 +9,6 @@ import (
 	"log"
 )
 
-var defaultHandlerTemplate = `
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Choose Your Own Adventure</title>
-</head>
-<body>
-    <h1>{{.Title}}</h1>
-    {{range .Paragraphs}}
-        <p>{{.}}</p>
-    {{end}}
-
-    <ul>
-    {{range .Options}}
-        <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
-    {{end}}
-    </ul>
-</body>`
-
 var templateDefault *template.Template
 
 func init() {
@@ -56,23 +36,47 @@ func JsonStory(r io.Reader) (Story, error) {
 	return story, nil
 }
 
-func NewHandler(s Story) handler {
-	return handler{story: s}
+type HandlerOption func(h *handler)
+
+func WithTemplate(t *template.Template) HandlerOption {
+	return func(h *handler) {
+		h.tpl = t
+	}
+}
+
+func WithPathFunc(fn func(r *http.Request) string) HandlerOption {
+	return func(h *handler) {
+		h.pathFn = fn
+	}
+}
+
+func NewHandler(s Story, opts ...HandlerOption) handler {
+	h := handler{story: s, tpl: templateDefault, pathFn: defaultPath}
+	for _, opt := range opts {
+		opt(&h)
+	}
+	return h
 }
 
 type handler struct {
-	story Story
+	story  Story
+	tpl    *template.Template
+	pathFn func(r *http.Request) string
 }
 
-func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func defaultPath(r *http.Request) string {
 	path := strings.TrimSpace(r.URL.Path)
 	if path == "" || path == "/" {
 		path = "/intro"
 	}
-	path = path[1:]
+	return path[1:]
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := h.pathFn(r)
 
 	if chapter, ok := h.story[path]; ok {
-		if err := templateDefault.Execute(w, chapter); err != nil {
+		if err := h.tpl.Execute(w, chapter); err != nil {
 			log.Printf("%v", err)
 			http.Error(w, "Something went wrong...", http.StatusInternalServerError)
 		}
